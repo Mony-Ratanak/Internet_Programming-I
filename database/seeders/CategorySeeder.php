38dbcf58534fdbd272ccd3fabc0c1dc1e2ba4cf6
MonyRatanak <hieklymonyratanak@gmail.com>
2024-01-17 09:44:26 +0700

    update

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            ['id' => 1, 'name' => 'Category 1'],
+            ['id' => 2, 'name' => 'Category 2'],
+            ['id' => 3, 'name' => 'Category 3'],
+        ]);
+    }
+}
